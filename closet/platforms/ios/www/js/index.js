691b68f919cbaf5002ce0380faa34d922714d5f6
James <james19961119@gmail.com>
2019-03-26 16:55:50 +0800

    listview delete button success

--- closet/platforms/ios/www/js/index.js
+++ closet/platforms/ios/www/js/index.js
@@ -1,55 +1,97 @@
 // 裝置準備完成
 function onDeviceReady() {
-    console.log("onDeviceReady 裝置準備完成!");
+  console.log("onDeviceReady 裝置準備完成!");
 }
 
 function onLoad() {
-    console.log("啟動 onLoad");
-    document.addEventListener("deviceready", onDeviceReady, false);
+  console.log("啟動 onLoad");
+  document.addEventListener("deviceready", onDeviceReady, false);
 
-    $(document).on("pagecreate", "#login", function () {
+  $(document).on("pagecreate", "#login", function() {});
 
-
+  $(document).on("pagecreate", "#page_second", function() {
+    var catagory_array = [];
+    $("#add_confirm").click(function() {
+      var type = $("#cloth_type").val();
+      console.log(type);
+      if (window.localStorage.getItem("catagory_array") === null) {
+        catagory_array = [type];
+      } else {
+        catagory_array = JSON.parse(
+          window.localStorage.getItem("catagory_array")
+        );
+        catagory_array.push(type);
+      }
+      window.localStorage.setItem(
+        "catagory_array",
+        JSON.stringify(catagory_array)
+      );
     });
 
-    $(document).on('pagecreate', '#page_second', function () {
-        var catagory_array = [];
-        $("#add_confirm").click(function () {
-            var type = $("#cloth_type").val();
-            console.log(type);
-            if (window.localStorage.getItem('catagory_array') === null) {
-                catagory_array = [type];
-            } else {
-                catagory_array = JSON.parse(window.localStorage.getItem('catagory_array'));
-                catagory_array.push(type);
-            }
-            window.localStorage.setItem("catagory_array", JSON.stringify(catagory_array));
+    $("#show_dialog").click(function() {
+      $.mobile.changePage("#add_catagory", {
+        transition: "pop",
+        role: "dialog"
+      });
+    });
+  });
 
-        });
+  $(document).on("pageshow", "#page_second", function() {
+    var catagory_array = [],
+      listview_html = "";
+    // window.localStorage.removeItem('catagory_array');
+    if (!(window.localStorage.getItem("catagory_array") === null)) {
+      catagory_array = JSON.parse(
+        window.localStorage.getItem("catagory_array")
+      );
+      for (var index in catagory_array) {
+        listview_html +=
+          '<li><a href="#" class="catagory" data-content="' +
+          catagory_array[index] +
+          '">' +
+          catagory_array[index] +
+          '</a><a href="#" class="delete" data-content="' +
+          catagory_array[index] +
+          '">Delete</a></li>';
+      }
+      $("#listview")
+        .html(listview_html)
+        .listview("refresh");
+    }
 
-        $('#show_dialog').click(function () {
-            $.mobile.changePage('#add_catagory', { transition: 'pop', role: 'dialog' });
-        });
+    $("a.catagory").click(function() {
+      var content = $(this).data("content");
+      console.log(content);
     });
 
-    $(document).on('pageshow', '#page_second', function () {
-        var catagory_array = [],
-            listview_html = '';
-        // window.localStorage.removeItem('catagory_array');
-        if (!(window.localStorage.getItem('catagory_array') === null)) {
-            catagory_array = JSON.parse(window.localStorage.getItem('catagory_array'));
-            for (var index in catagory_array) {
-                listview_html += '<li><a href="#" class="catagory" data-split-icon="delete" data-content="' + catagory_array[index] + '">' + catagory_array[index] + '</a><a href="#" class="delete">Delete</a></li>';
-            }
-            $('#listview').html(listview_html).listview('refresh');
-        }
-
-        $('a.catagory').click(function () {
-            var content = $(this).data('content');
-            console.log(content);
-        });
-
+    $("a.delete").click(function() {
+      var content = $(this).data("content");
+      navigator.notification.confirm(
+        "您確定要刪除 " + content + " 的相簿？",
+        function(buttonindex) {
+          if (buttonindex === 2) {
+            catagory_array = JSON.parse(
+              window.localStorage.getItem("catagory_array")
+            );
+            remove(catagory_array, content);
+            window.localStorage.setItem(
+              "catagory_array",
+              JSON.stringify(catagory_array)
+            );
+            $("#page_second").trigger("pageshow");
+          }
+        },
+        "刪除提示",
+        ["保留", "刪除"]
+      );
     });
+  });
+
+  function remove(array, element) {
+    const index = array.indexOf(element);
+
+    if (index !== -1) {
+      array.splice(index, 1);
+    }
+  }
 }
-
-
